cee28f9bdaf330293763b6612b0e406b01fea38b
Tobias Öberg <tobias.oberg@iths.se>
2025-04-08 11:55:47 +0200

    Lab update

---
+++ Lab_2/filmrecommender.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import numpy as np
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import streamlit as st
+import requests
+
+#my_api_key = "f43b87de9064ce5282fc85bc77c5d2cc"
+
+@st.cache_data # cachar data så att det inte laddas om varje gång
+def load_data():
+    movies = pd.read_csv(r"C:\Code\ML-Tobias-Oberg-AI24\Lab_2\movies.csv")
+    tags = pd.read_csv(r"C:\Code\ML-Tobias-Oberg-AI24\Lab_2\tags.csv")
+    
+    return movies, tags
+
+# Rensar specialtecken och behåller endast bokstäver och siffror
+# https://www.geeksforgeeks.org/python-remove-all-characters-except-letters-and-numbers/
+def clean_data(text):
+        return re.sub("[^a-zA-Z0-9]"," ", text).strip()
+
+
+def clean_combine_data(movies, tags):
+    # Gruppera och slå ihop tags
+    tags_grouped = tags.groupby("movieId")["tag"].apply(lambda x: " ".join(x.drop_duplicates().astype(str))).reset_index()
+    movies = movies.merge(tags_grouped, on="movieId", how="left")
+   
+    # Fyll i NaN-värden med tomma strängar
+    movies["tag"] = movies["tag"].fillna("")
+    movies["genres"] = movies["genres"].fillna("")
+    
+    # Rensa texten i title, tag och genres, använder clean_data - funktionen 
+    movies["title"] = movies["title"].apply(clean_data)
+    movies["tag"] = movies["tag"].apply(clean_data)
+    
+    
+    movies["genres"] = movies["genres"].apply(clean_data)
+    
+    # Kombinera genres och tags 
+    movies["combined_genres_tag"] = movies["genres"] + " " + movies["tag"]
+
+    return movies
+
+
+
+
+# Skapa en TF-IDF-matris med n-gram och stopwords
+@st.cache_resource # cachar resursen så att den inte laddas om varje gång
+def vectorizer_matrix(movies):
+   vectorizer = TfidfVectorizer(stop_words="english", ngram_range=(1,2))
+   tfidf = vectorizer.fit_transform(movies["combined_genres_tag"]) 
+   return vectorizer, tfidf
+
+
+     
+# Beräkna likheten mellan en given film och alla andra filmer i datasetet
+# Använd cosine similarity för att beräkna likheten mellan TF-IDF-vektorerna
+# Stort dataset så istället jämförs filmerna 1 mot 1
+def similar_movies(tfidf, vectorizer, query, movies):
+    query = clean_data(query) 
+    query_vec = vectorizer.transform([query]) 
+
+    sim_score = cosine_similarity(query_vec, tfidf).flatten() # flatten gör att den blir en array istället för en matris
+    indices = np.argsort(sim_score)[-5:][::-1] # sorterar i descending order och tar de 5 mest lika filmerna
+    results = movies.iloc[indices] # hämtar de 5 mest lika filmerna från movies dataframe
+
+    return results[["title"]] # returnerar title för de 5 mest lika filmerna
+
+
+st.markdown(" # Filmrekommendationer")
+st.write("Sök efter en film för att få rekommendationer")
+
+
+movies, tags = load_data() 
+movies = clean_combine_data(movies,tags) 
+vectorizer, tfidf = vectorizer_matrix(movies) 
+
+movie_name = st.text_input("Skriv in en film: :movie_camera:", "")
+
+
+
+
+if movie_name:
+        results = similar_movies(tfidf, vectorizer, movie_name, movies)
+        results = results.rename(columns={"title": "Titlar:"})
+        st.write("Här är 5 liknande filmer:")
+        st.dataframe(results, hide_index=True)
+        
+       
+
+
+
+                       
+
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# Machine-Learning-Tobias-Oberg-AI24
-Machine-learning, labs/code-alongs/exercises
+# Körning av applikationen
+Länk till webapplikation: 
+skriv in streamlit run *filnamn.py* i terminalen för att köra den lokalt. 
+
+
+
+## Metoder
+Jag använder TFIDF-vektorisering(Term Frequency-Inverse Document Frequency) för att göra om text till numeriska värden.
+Med hjälp av stopwords = "english" så tas vanliga ord bort så som "the" och "i" i engelska språket. Bigrams(n_grams(1,2)) används för att få med två ord som är sammanhörande. Detta gör att ord som till exempel "Science Fiction" analyseras som en fras och inte två separata ord. 
+
+För att jämföra vektorerna som skapas med hjälp av TFIDF så använder jag cosine similarity. Cosine similarity mäter hur lika två filmer är. Alltså, ju högre similaritet desto mer lika är filmerna. De 5 filmer med högst similaritet returneras. 
+
+
+### Begränsningar
+Applikationen använder ett dataset för filmer och ett annat för taggar. Om information saknas, så som taggar eller om datasetet innehåller felaktig data så gör detta att rekommendationerna har mindre träffsäkerhet. 
+
+TF-IDF beräknar likheten mellan genrer och taggar. Om det inte finns tillräckligt beskrivande taggar eller genrer så uppstår en begränsning. Beräkningen tar inte hänsyn till andra faktorer som kan spela stor roll, till exempel skådespelare eller regissör som bara förekommer i vissa fall. 
+
+En annan begränsning som sker är att det inte finns någon beskrivning om filmerna, detta leder till att många filmer klassificeras som liknande filmer på grund av likheten mellan genrer och taggar. 
+
+
+#### Val
+Jag använder pandas för datahantering, numpy för numeriska beräkningar, scikit-learn för tfidf och cosine similarity och streamlit för att köra applikationen på en websida då jag ville testa något nytt som liknar en dash-applikation. 
+TFIDF och Cosine similarity valdes då den fick bäst resultat, euclidian distance testades men upplevdes sämre. 
+Att slå ihop genrer och taggar fungerade bättre än att kolla på båda separat. Att lägga till titel testades men resultatet påverkades för mycket. 
+
+
+
+
+
